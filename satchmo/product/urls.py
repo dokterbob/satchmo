d376fcaf8951c4bfe050812c8e350b35e9446928
John Shaffer <jshaffer2112@gmail.com>
2007-11-10 23:56:19 +0000

    Indent product URLs.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%40884

--- satchmo/product/urls.py
+++ satchmo/product/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('satchmo.product',
-(r'^(?P<product_slug>[-\w]+)/prices/$', 'views.get_price', {}, 'satchmo_product_prices'),
-(r'^(?P<product_slug>[-\w]+)/$', 'views.get_product', {}, 'satchmo_product'),
-(r'^inventory/edit/$', 'adminviews.edit_inventory', {}, 'satchmo_admin_edit_inventory'),
-(r'^inventory/export/$', 'adminviews.export_products', {}, 'satchmo_admin_product_export'),
-(r'^inventory/import/$', 'adminviews.import_products', {}, 'satchmo_admin_product_import'),
-(r'^inventory/report/$', 'adminviews.product_active_report', {}, 'satchmo_admin_product_report'),
+    (r'^(?P<product_slug>[-\w]+)/$', 'views.get_product', {}, 'satchmo_product'),
+    (r'^(?P<product_slug>[-\w]+)/prices/$', 'views.get_price', {}, 'satchmo_product_prices'),
+    (r'^inventory/edit/$', 'adminviews.edit_inventory', {}, 'satchmo_admin_edit_inventory'),
+    (r'^inventory/export/$', 'adminviews.export_products', {}, 'satchmo_admin_product_export'),
+    (r'^inventory/import/$', 'adminviews.import_products', {}, 'satchmo_admin_product_import'),
+    (r'^inventory/report/$', 'adminviews.product_active_report', {}, 'satchmo_admin_product_report'),
 )
