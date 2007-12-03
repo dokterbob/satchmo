6363d28e58ed54b1aa199fa4d03415dedd35bb63
Bruce Kroeze <bkroeze@gmail.com>
2007-12-03 04:15:27 +0000

    added new, locale-aware moneyfmt in l10n/utils
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%40912

---
+++ satchmo/l10n/utils.py
@@ -0,0 +1,87 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import get_language, to_locale
+from satchmo.configuration import config_value
+import locale
+import logging
+
+log = logging.getLogger('l10n.utils')
+
+def get_locale_conv(loc=None):
+    startloc = loc
+    if not loc:
+        loc = to_locale(get_language())
+
+    # '-' is a language delimiter, not a locale, but people often mess that up
+    if loc.find('-') > -1:
+        loc = to_locale(loc)
+
+    try:
+        log.debug('setting locale: %s', loc)
+        locale.setlocale(locale.LC_ALL, locale.normalize(loc))
+        return locale.localeconv()
+    except locale.Error:
+        # darn, try a different path
+        pos = loc.find('_')
+        if pos>-1:
+            loc = loc[:pos]
+            return get_locale_conv(loc)
+        else:
+            loc = settings.LANGUAGE_CODE
+            if loc != startloc:
+                log.warn("Cannot set locale to '%s', using default locale '%s'", startloc, loc)
+                return get_locale_conf(loc)
+        
+            else:
+                log.fatal("Cannot set locale to default locale '%s', something is misconfigured", loc)
+                raise ImproperlyConfigured("Bad settings LANGUAGE_CODE")
+                
+def moneyfmt(val, curr=None, places=-1, grouping=True, wrapcents='', current_locale=None):
+    """Formats val according to the currency settings in the current locale.
+    Ported-and-modified from Python 2.5
+    """
+    conv = get_locale_conv(current_locale)
+
+    if places < 0:
+        places = conv['int_frac_digits']
+    s = locale.format('%%.%if' % places, abs(val), grouping, monetary=True)
+    # '<' and '>' are markers if the sign must be inserted between symbol and value
+    s = '<' + s + '>'
+
+    if curr is None:
+        curr = config_value('SHOP', 'CURRENCY')
+    precedes = conv[val<0 and 'n_cs_precedes' or 'p_cs_precedes']
+    separated = conv[val<0 and 'n_sep_by_space' or 'p_sep_by_space']
+
+    if precedes:
+        s = curr + (separated and ' ' or '') + s
+    else:
+        s = s + (separated and ' ' or '') + curr
+
+    sign_pos = conv[val<0 and 'n_sign_posn' or 'p_sign_posn']
+    sign = conv[val<0 and 'negative_sign' or 'positive_sign']
+
+    if sign_pos == 0:
+        s = '(' + s + ')'
+    elif sign_pos == 1:
+        s = sign + s
+    elif sign_pos == 2:
+        s = s + sign
+    elif sign_pos == 3:
+        s = s.replace('<', sign)
+    elif sign_pos == 4:
+        s = s.replace('>', sign)
+    else:
+        # the default if nothing specified;
+        # this should be the most fitting sign position
+        s = sign + s
+
+    val = s.replace('<', '').replace('>', '')
+
+    if wrapcents:
+        pos = s.rfind(conf['decimal_point'])
+        if pos>-1:
+            pos +=1 
+            val = u"%s<%s>%s</%s>" % val[:pos], wrapcents, val[pos:], wrapcents
+
+    return val
--- satchmo/shop/templatetags/satchmo_currency.py
+++ satchmo/shop/templatetags/satchmo_currency.py
@@ -4,28 +4,19 @@ from django.conf import settings
 from django.utils.encoding import force_unicode
 from satchmo.configuration import config_value
 from satchmo.shop.templatetags import get_filter_args
+from satchmo.l10n.utils import moneyfmt
 try:
     from django.utils.safestring import mark_safe
 except ImportError:
     mark_safe = lambda s:s
     
-#The moneyfmt script was taken from the Python decimal recipes.
-#See it here - http://docs.python.org/lib/decimal-recipes.html
-
 register = template.Library()
-
 
 def currency(value, args=""):
     """Convert a value to a money formatted string.
 
     places:  required number of places after the decimal point
     curr:    optional currency symbol before the sign (may be blank)
-    sep:     optional grouping separator (comma, period, space, or blank)
-    dp:      decimal point indicator (comma or period)
-             only specify as blank when places is zero
-    pos:     optional sign for positive numbers: '+', space or blank
-    neg:     optional sign for negative numbers: '-', '(', space or blank
-    trailneg:optional trailing minus indicator:  '-', ')', space or blank
     wrapcents:tag to wrap the part after the decimal point
 
     Usage:
@@ -38,80 +29,12 @@ def currency(value, args=""):
         return value
 
     args, kwargs = get_filter_args(args, 
-        keywords=('places','curr','sep','dp','pos','neg','trailneg','wrapcents'),
+        keywords=('places','curr', 'wrapcents'),
         intargs=('places',), stripquotes=True)
 
     value = Decimal(str(value))
         
     return mark_safe(moneyfmt(value, **kwargs))
-
-def moneyfmt(value, places=2, curr=None, sep=',', dp='.',
-             pos='', neg='-', trailneg='', wrapcents=''):
-    """Convert Decimal to a money formatted string.
-
-    places:  required number of places after the decimal point
-    curr:    optional currency symbol before the sign (may be blank)
-    sep:     optional grouping separator (comma, period, space, or blank)
-    dp:      decimal point indicator (comma or period)
-             only specify as blank when places is zero
-    pos:     optional sign for positive numbers: '+', space or blank
-    neg:     optional sign for negative numbers: '-', '(', space or blank
-    trailneg:optional trailing minus indicator:  '-', ')', space or blank
-    wrapcents:tag to wrap the part after the decimal point
-
-    >>> d = Decimal('-1234567.8901')
-    >>> moneyfmt(d, curr='$')
-    '-$1,234,567.89'
-    >>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
-    '1.234.568-'
-    >>> moneyfmt(d, curr='$', neg='(', trailneg=')')
-    '($1,234,567.89)'
-    >>> moneyfmt(Decimal(123456789), sep=' ')
-    '123 456 789.00'
-    >>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
-    '<.02>'
-
-    """
-    if curr is None:
-        curr = config_value('SHOP', 'CURRENCY')
-
-    q = Decimal((0, (1,), -places))    # 2 places --> '0.01'
-    sign, digits, exp = value.quantize(q).as_tuple()
-    assert exp == -places    
-    result = []
-    digits = map(str, digits)
-    build, next = result.append, digits.pop
-    if sign:
-        build(trailneg)
-        
-    if wrapcents and places > 0:
-        build("</%s>" % wrapcents)
-    
-    for i in range(places):
-        if digits:
-            build(next())
-        else:
-            build('0')
-
-    if wrapcents and places > 0:
-        build("<%s>" % wrapcents)
-            
-    build(dp)
-    
-    i = 0
-    while digits:
-        build(next())
-        i += 1
-        if i == 3 and digits:
-            i = 0
-            build(sep)
-    build(force_unicode(curr))
-    if sign:
-        build(neg)
-    else:
-        build(pos)
-    result.reverse()
-    return u''.join(result)
 
 register.filter('currency', currency)
 currency.is_safe = True
