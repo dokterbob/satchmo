34fafd0bf3f2da5061af78ad26a706a152f61ad9
Chris Moffitt <chris@moffitts.net>
2008-02-01 04:09:59 +0000

    Update settings to show what needs to be used in windows for lang settings.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401025

--- satchmo/settings-customize.py
+++ satchmo/settings-customize.py
@@ -32,6 +32,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
+# For windows, you must use 'us' instead
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
