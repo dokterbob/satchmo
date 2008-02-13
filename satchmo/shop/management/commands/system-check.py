c94378696f92a73a7094cfbab0f2073c217f8491
Chris Moffitt <chris@moffitts.net>
2008-02-13 02:52:59 +0000

    Initial checkin of a system-check utility to hopefully ease the setup and install of Satchmo.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401033

---
+++ satchmo/shop/management/commands/system-check.py
@@ -0,0 +1,52 @@
+from django.core.management.base import NoArgsCommand
+import sys
+from decimal import Decimal
+
+class Command(NoArgsCommand):
+    help = "Check the system to see if the Satchmo components are installed correctly."
+    
+    def handle_noargs(self, **options):
+        errors = []
+        print "Checking your satchmo configuration."
+        try:
+            import satchmo
+        except ImportError:
+            errors.append("Satchmo is not installed correctly. Please verify satchmo is on your sys path.")
+        try:
+            import Crypto.Cipher
+        except ImportError:
+            errors.append("The Python Cryptography Toolkit is not installed.")
+        try:
+            import Image
+        except ImportError:
+            errors.append("The Python Imaging Library is not installed.")
+        try:
+            import elementtree
+        except ImportError:
+            errors.append("Elementtree is not installed.")
+        try:
+            import reportlab
+        except ImportError:
+            errors.append("Reportlab is not installed.")
+        try:
+            import trml2pdf
+        except ImportError:
+            errors.append("Tiny RML2PDF is not installed.")
+        try:
+            import comment_utils
+        except ImportError:
+            errors.append("Django comment_utils is not installed.")
+        try:
+            import registration
+        except ImportError:
+            errors.append("Django registration is not installed.")
+            
+        python_ver = Decimal("%s.%s" % (sys.version_info[0], sys.version_info[1]))
+        if python_ver < Decimal("2.4"):
+            errors.append("Python version must be at least 2.4.")
+        if len(errors) == 0:
+            print "Your configuration has no errors."
+        else:
+            print "The following errors were found:"
+            for error in errors:
+                print error
