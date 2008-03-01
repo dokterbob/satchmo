b990843e230787dd6a46226f7c1ba40c3e575772
Chris Moffitt <chris@moffitts.net>
2008-03-01 21:38:50 +0000

    Improved design of product ratings so it only tries to load it if it is configured. Closes #270. Thanks Dirk
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401056

---
+++ satchmo/shop/templatetags/satchmo_ratings.py
@@ -0,0 +1,15 @@
+import logging
+from django.template import Library
+
+log = logging.getLogger('shop.templatetags')
+
+register = Library()
+
+@register.inclusion_tag('_render_product_ratings.html', takes_context=True)
+def product_ratings(context):
+    shop = context['shop']
+    rendered = ''
+    if shop.options.SHOP.RATINGS.value:
+        from django.template.loader import render_to_string
+        rendered = render_to_string('_product_ratings.html', context)
+    return { 'rendered_product_ratings': rendered }
