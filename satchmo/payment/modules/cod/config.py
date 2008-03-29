e613b6d2ef37448ac0237e02a32f2cb314e2c67c
Chris Moffitt <chris@moffitts.net>
2008-03-29 14:13:05 +0000

    Add a LIVE config entry for COD module. Closes #303. Thanks.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401102

--- satchmo/payment/modules/cod/config.py
+++ satchmo/payment/modules/cod/config.py
@@ -18,6 +18,13 @@ config_register([
         description=_("Use SSL for the module checkout pages?"), 
         default=False),
         
+    BooleanValue(PAYMENT_GROUP, 
+        'LIVE', 
+        description=_("Accept real payments"),
+        help_text=_("False if you want to be in test mode"),
+        default=False),
+
+        
     ModuleValue(PAYMENT_GROUP,
         'MODULE',
         description=_('Implementation module'),
