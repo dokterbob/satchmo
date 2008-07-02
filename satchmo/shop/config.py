d1f571dfb8e0c9bb84c2e407854409593ff198b5
John Shaffer <jshaffer2112@gmail.com>
2008-07-02 04:06:19 +0000

    Move sample-logo.bmp from templates/ to static/ so that the template directory doesn't need to be in the project directory.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401302

--- satchmo/shop/config.py
+++ satchmo/shop/config.py
@@ -1,17 +1,15 @@
 ﻿# -*- coding: utf-8 -*-
 
-import urlparse
 import os
-from satchmo.configuration import config_register, BooleanValue, StringValue, MultipleStringValue, SHOP_GROUP, ConfigurationGroup, PositiveIntegerValue
-from django.utils.translation import ugettext_lazy as _
+import urlparse
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from satchmo.configuration import config_register, BooleanValue, StringValue, MultipleStringValue, SHOP_GROUP, ConfigurationGroup, PositiveIntegerValue
 
-install_path = ((hasattr(settings, 'SATCHMO_DIRNAME') and settings.SATCHMO_DIRNAME) or \
-                settings.DIRNAME)
 default_icon_url = urlparse.urlunsplit(
     ('file',
      '',
-     os.path.join(install_path, 'templates/pdf/sample-logo.bmp'),
+     os.path.join(settings.MEDIA_ROOT, 'images/sample-logo.bmp'),
      '',
      '')
     )
@@ -19,18 +17,18 @@ default_icon_url = urlparse.urlunsplit(
 #### SHOP Group ####
 
 CURRENCY = config_register(
-    StringValue(SHOP_GROUP, 
-        'CURRENCY', 
+    StringValue(SHOP_GROUP,
+        'CURRENCY',
         description= _("Default currency symbol"),
         help_text= _("Use a '_' character to force a space."),
         default="$"))
 
 ENABLE_RATINGS = config_register(
-    BooleanValue(SHOP_GROUP, 
-        'RATINGS', 
-        description= _("Enable product ratings"), 
+    BooleanValue(SHOP_GROUP,
+        'RATINGS',
+        description= _("Enable product ratings"),
         default=True))
-        
+
 RANDOM_FEATURED = config_register(
     BooleanValue(SHOP_GROUP,
         'RANDOM_FEATURED',
@@ -65,46 +63,46 @@ LOGO_URI = config_register(
                    "file:///var/www/html/images/logo.jpg")),
     default = default_icon_url
 ))
-                
+
 #### Google Group ####
 
 GOOGLE_GROUP = ConfigurationGroup('GOOGLE', _('Google Settings'))
 
 GOOGLE_ANALYTICS = config_register(
-    BooleanValue(GOOGLE_GROUP, 
-        'ANALYTICS', 
-        description= _("Enable Analytics"), 
+    BooleanValue(GOOGLE_GROUP,
+        'ANALYTICS',
+        description= _("Enable Analytics"),
         default=False,
         ordering=0))
 
 GOOGLE_USE_URCHIN = config_register(
-    BooleanValue(GOOGLE_GROUP, 
-        'USE_URCHIN', 
-        description= _("Use Urchin?"), 
+    BooleanValue(GOOGLE_GROUP,
+        'USE_URCHIN',
+        description= _("Use Urchin?"),
         help_text=_("Use the old-style, urchin javascript?.  This is not needed unless your analytics account hasn't been updated yet."),
         default = False,
         ordering=5,
         requires = GOOGLE_ANALYTICS))
-        
+
 GOOGLE_ANALYTICS_CODE = config_register(
-    StringValue(GOOGLE_GROUP, 
-        'ANALYTICS_CODE', 
-        description= _("Analytics Code"), 
+    StringValue(GOOGLE_GROUP,
+        'ANALYTICS_CODE',
+        description= _("Analytics Code"),
         default = "",
         ordering=5,
         requires = GOOGLE_ANALYTICS))
-    
+
 GOOGLE_ADWORDS = config_register(
-    BooleanValue(GOOGLE_GROUP, 
-        'ADWORDS', 
-        description= _("Enable Conversion Tracking"), 
+    BooleanValue(GOOGLE_GROUP,
+        'ADWORDS',
+        description= _("Enable Conversion Tracking"),
         default=False,
         ordering=10))
-        
+
 GOOGLE_ADWORDS_ID = config_register(
-    StringValue(GOOGLE_GROUP, 
-        'ADWORDS_ID', 
-        description= _("Adwords ID (ex: UA-abcd-1)"), 
+    StringValue(GOOGLE_GROUP,
+        'ADWORDS_ID',
+        description= _("Adwords ID (ex: UA-abcd-1)"),
         default = "",
         ordering=15,
         requires = GOOGLE_ADWORDS))
@@ -133,4 +131,4 @@ LANGUAGES_AVAILABLE = config_register(
             ('pt-br',"Português"),
             ('bg',"Български")]
     ))
-    
+
