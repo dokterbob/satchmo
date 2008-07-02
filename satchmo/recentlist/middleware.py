28562bc5ea9eaacf37915b3cda75e7aadc5c147a
John Shaffer <jshaffer2112@gmail.com>
2008-07-02 02:57:16 +0000

    Clean up recentlist/middleware.py.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401299

--- satchmo/recentlist/middleware.py
+++ satchmo/recentlist/middleware.py
@@ -5,9 +5,10 @@ import re
 
 log = logging.getLogger('recentlist.middleware')
 
-producturl = urlresolvers.reverse('satchmo_product', None,('FAKE',))
+producturl = urlresolvers.reverse('satchmo_product',
+    kwargs={'product_slug': 'FAKE'})
 producturl = "^" + producturl.replace("FAKE", r'(?P<slug>[-\w]+)')
-log.debug('product url is %s', producturl)
+log.debug('Product url is %s', producturl)
 
 urlre = re.compile(producturl)
 
@@ -16,14 +17,14 @@ class RecentProductMiddleware(object):
     def process_response(self, request, response):
         g = urlre.search(request.path)
         if g and len(g.groups()) > 0 and "/admin/" not in request.path:
-            recentmax = config_value('SHOP','RECENT_MAX')+1
+            recentmax = config_value('SHOP', 'RECENT_MAX') + 1
             slug = g.groups()[0]
-            recent = request.session.get('RECENTLIST',[])
+            recent = request.session.get('RECENTLIST', [])
             if slug not in recent:
                 recent.insert(0, slug)
                 if len(recent) > recentmax:
                     recent = recent[:recentmax]
                 log.debug('Added recently viewed: %s', recent)
                 request.session['RECENTLIST'] = recent
-            
+
         return response
