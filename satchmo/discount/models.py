c91849fb6cd7fd9008795c7e477b82e54b7f58fe
John Shaffer <jshaffer2112@gmail.com>
2008-07-09 04:50:23 +0000

    Clean up discount/models.py.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401321

--- satchmo/discount/models.py
+++ satchmo/discount/models.py
@@ -2,18 +2,19 @@
 Sets up a discount that can be applied to a product
 """
 
-from datetime import date
 try:
-    from decimal import Decimal, getcontext
+    from decimal import Decimal
 except:
-    from django.utils._decimal import Decimal, getcontext
-from django.db import models
-from django.utils.translation import ugettext, ugettext_lazy as _
-from satchmo.product.models import Product
-from satchmo.shop.templatetags.satchmo_currency import moneyfmt
-from satchmo.shop.utils.validators import MutuallyExclusiveWithField
+    from django.utils._decimal import Decimal
+
+import datetime
 import operator
 import logging
+from django.db import models
+from django.utils.translation import ugettext, ugettext_lazy as _
+from satchmo.l10n.utils import moneyfmt
+from satchmo.product.models import Product
+from satchmo.shop.utils.validators import MutuallyExclusiveWithField
 
 log = logging.getLogger('Discount.models')
 
@@ -21,17 +22,17 @@ percentage_validator = MutuallyExclusiveWithField('amount')
 amount_validator = MutuallyExclusiveWithField('percentage')
 
 class NullDiscount(object):
-    
+
     def __init__(self):
         self.description = _("No Discount")
         self.total = Decimal("0.00")
         self.item_discounts = {}
         self.discounted_prices = []
         self.automatic = False
-        
+
     def calc(self, *args):
         return Decimal("0.00")
-                
+
 class Discount(models.Model):
     """
     Allows for multiple types of discounts including % and dollar off.
@@ -47,7 +48,7 @@ class Discount(models.Model):
         max_digits=4, blank=True, null=True,
         validator_list=[percentage_validator],
         help_text=_("Enter absolute discount amount OR percentage.  Percentage example: \"0.10\"."))
-    automatic = models.BooleanField(_("Is this an automatic discount?"), default=False, blank=True, 
+    automatic = models.BooleanField(_("Is this an automatic discount?"), default=False, blank=True,
         null=True, help_text=_("Use this field to advertise the discount on all products to which it applies.  Generally this is used for site-wide sales."))
     allowedUses = models.IntegerField(_("Number of allowed uses"),
         blank=True, null=True, help_text=_('Not implemented.'))
@@ -62,7 +63,7 @@ class Discount(models.Model):
         help_text=_("Should this discount remove all shipping costs?"))
     includeShipping = models.BooleanField(_("Include shipping"), blank=True, null=True,
         help_text=_("Should shipping be included in the discount calculation?"))
-    validProducts = models.ManyToManyField(Product, verbose_name=_("Valid Products"), filter_interface=True, 
+    validProducts = models.ManyToManyField(Product, verbose_name=_("Valid Products"), filter_interface=True,
         blank=True, null=True, help_text="Make sure not to include gift certificates!")
 
     def __init__(self, *args, **kwargs):
@@ -79,9 +80,9 @@ class Discount(models.Model):
         """
         if not self.active:
             return (False, ugettext('This coupon is disabled.'))
-        if self.startDate > date.today():
+        if self.startDate > datetime.date.today():
             return (False, ugettext('This coupon is not active yet.'))
-        if self.endDate < date.today():
+        if self.endDate < datetime.date.today():
             return (False, ugettext('This coupon has expired.'))
         if self.numUses > self.allowedUses:
             return (False, ugettext('This discount has exceeded the number of allowed uses.'))
@@ -101,7 +102,7 @@ class Discount(models.Model):
                     break   #Once we have 1 valid item, we exit
         else:
             validItems = True
-            
+
         if validItems:
             return (True, ugettext('Valid.'))
         else:
@@ -119,39 +120,39 @@ class Discount(models.Model):
         validproducts = self._get_valid_product_dict()
         allvalid = len(validproducts) == 0
         ordertotal = Decimal("0.00")
-            
+
         for lineitem in order.orderitem_set.all():
             lid = lineitem.id
             price = lineitem.line_item_price
             if allvalid or lineitem.product.id in validproducts:
                 discounted[lid] = price
                 ordertotal += price
-        
+
         if self.includeShipping and not self.freeShipping:
             shipcost = order.shipping_cost
             discounted['Shipping'] = shipcost
             ordertotal += shipcost
-    
+
         if self.amount:
             # perform a flat rate discount, applying evenly to all items
             # in cart
-            discounted = apply_even_split(discounted, self.amount)        
-        
+            discounted = apply_even_split(discounted, self.amount)
+
         elif self.percentage:
             discounted = apply_percentage(discounted, self.percentage)
-        
+
         else:
             # no discount, probably shipping-only
             zero = Decimal("0.00")
             for key in discounted.keys():
                 discounted[key] = zero
-            
+
         if self.freeShipping:
             shipcost = order.shipping_cost
             discounted['Shipping'] = shipcost
             ordertotal += shipcost
-            
-        self._item_discounts = discounted        
+
+        self._item_discounts = discounted
         self._calculated = True
 
     def _total(self):
@@ -164,10 +165,10 @@ class Discount(models.Model):
         assert(self._calculated)
         return self._item_discounts
     item_discounts = property(_item_discounts)
-    
+
     def _percentage_text(self):
         """Get the human readable form of the sale percentage."""
-        if self.percentage > 1:        
+        if self.percentage > 1:
             pct = self.percentage
         else:
             pct = self.percentage*100
@@ -176,7 +177,7 @@ class Discount(models.Model):
         return "%i%%" % pct
 
     percentage_text = property(_percentage_text)
-    
+
     def valid_for_product(self, product):
         """Tests if discount is valid for a single product"""
         p = self.validProducts.filter(id__exact = product.id)
@@ -193,8 +194,8 @@ def apply_even_split(discounted, amount):
     log.debug('Apply even split on %s to: %s', amount, discounted)
     lastct = -1
     ct = len(discounted)
-    split_discount = amount/ct    
-    
+    split_discount = amount/ct
+
     while ct > 0:
         log.debug("Trying with ct=%i", ct)
         delta = Decimal("0.00")
@@ -209,18 +210,18 @@ def apply_even_split(discounted, amount):
                 delta += price
                 applied += price
                 ct -= 1
-        
+
         if applied >= amount - Decimal("0.01"):
             ct = 0
-        
+
         if ct == lastct:
             ct = 0
         else:
             lastct = ct
-        
+
         if ct > 0:
             split_discount = (amount-delta)/ct
-    
+
     round_cents(work)
     return work
 
@@ -229,7 +230,7 @@ def apply_percentage(discounted, percentage):
     if percentage > 1:
         log.warn("Correcting discount percentage, should be less than 1, is %s", percentage)
         percentage = percentage/100
-        
+
     for lid, price in discounted.items():
         work[lid] = price * percentage
     round_cents(work)
