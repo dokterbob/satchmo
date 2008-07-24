a577f77d3a9709ab973f54fdd93d6e09354311da
Chris Moffitt <chris@moffitts.net>
2008-07-24 02:42:37 +0000

    Add support for bidi languages. Closes #453
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401356

--- satchmo/settings-customize.py
+++ satchmo/settings-customize.py
@@ -79,6 +79,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'satchmo.recentlist.context_processors.recent_products',
     'satchmo.shop.context_processors.settings',
+    'django.core.context_processors.i18n'
 )
 
 ROOT_URLCONF = 'satchmo.urls'
