5882dde87a92806f0c85708934e2b2a693489213
Michal Salaban <michal@salaban.info>
2008-08-22 17:52:10 +0000

    Fixed #498 and update to match Django trunk: Send one-time link, not a new password if user has forgotten it.
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401435

--- satchmo/accounts/urls.py
+++ satchmo/accounts/urls.py
@@ -45,4 +45,6 @@ urlpatterns += patterns('django.contrib.auth.views',
     (r'^password_reset/done/$', 'password_reset_done', {'template_name':'registration/password_reset_done.html'}, 'auth_password_reset_done'),
     (r'^password_change/$', 'password_change', {'template_name':'registration/password_change_form.html'}, 'auth_password_change'),
     (r'^password_change/done/$', 'password_change_done', {'template_name':'registration/password_change_done.html'}, 'auth_change_done'),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm'),
+    (r'^reset/done/$', 'password_reset_complete'),
 )
