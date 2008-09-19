7acf74d2cbf1d7e70ab2346b06bf984e356a8ed3
Bruce Kroeze <bkroeze@gmail.com>
2008-09-19 05:54:39 +0000

    Adding optional parameter to caching helper functions to allow them to raise errors instead of returning None
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401531

--- satchmo/caching/models.py
+++ satchmo/caching/models.py
@@ -32,7 +32,7 @@ class CachedObjectMixin(object):
     def is_cached(self, *args, **kwargs):
         return caching.is_cached(self.cache_key(*args, **kwargs))
         
-def find_by_id(cls, groupkey, objectid):
+def find_by_id(cls, groupkey, objectid, raises=False):
     """A helper function to look up an object by id"""
     ob = None
     try:
@@ -44,11 +44,13 @@ def find_by_id(cls, groupkey, objectid):
 
         except cls.DoesNotExist:
             log.debug("No such %s: %s", groupkey, objectid)
+            if raises:
+                raise cls.DoesNotExist
 
     return ob
 
 
-def find_by_key(cls, groupkey, key):
+def find_by_key(cls, groupkey, key, raises=False):
     """A helper function to look up an object by key"""
     ob = None
     try:
@@ -60,10 +62,12 @@ def find_by_key(cls, groupkey, key):
 
         except cls.DoesNotExist:
             log.debug("No such %s: %s", groupkey, key)
+            if raises:
+                raise
 
     return ob
     
-def find_by_slug(cls, groupkey, slug):
+def find_by_slug(cls, groupkey, slug, raises=False):
     """A helper function to look up an object by slug"""
     ob = None
     try:
@@ -75,6 +79,8 @@ def find_by_slug(cls, groupkey, slug):
 
         except cls.DoesNotExist:
             log.debug("No such %s: %s", groupkey, slug)
+            if raises:
+                raise
 
     return ob
 
