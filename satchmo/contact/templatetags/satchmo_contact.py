fa70b5d9324de23bcd93cf2cef057179c2649bd8
Bruce Kroeze <bkroeze@gmail.com>
2008-09-28 06:52:25 +0000

    Added new filter contact_for_user
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401566

--- satchmo/contact/templatetags/satchmo_contact.py
+++ satchmo/contact/templatetags/satchmo_contact.py
@@ -7,3 +7,11 @@ def addressblock(address):
     return {"address" : address}
 
 register.inclusion_tag('contact/_addressblock.html')(addressblock)
+
+def contact_for_user(user):
+    if user.contact_set.count() > 0:
+        return user.contact_set.all()[0]
+    else:
+        return None
+
+register.filter('contact_for_user')(contact_for_user)
