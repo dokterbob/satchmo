708117cf130b464ba3108b4fcb1e1d1d08dc45c3
Bruce Kroeze <bkroeze@gmail.com>
2008-10-27 15:53:06 +0000

    added balance and paydate to purchaseorder
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401671

--- satchmo/payment/modules/purchaseorder/admin.py
+++ satchmo/payment/modules/purchaseorder/admin.py
@@ -2,7 +2,7 @@ from satchmo.payment.modules.purchaseorder.models import PurchaseOrder
 from django.contrib import admin
 
 class PurchaseOrderOptions(admin.ModelAdmin):
-    list_display = ('po_number', 'order_link')
+    list_display = ('po_number', 'balance_due', 'order_link')
 
 admin.site.register(PurchaseOrder, PurchaseOrderOptions)
 
--- satchmo/payment/modules/purchaseorder/models.py
+++ satchmo/payment/modules/purchaseorder/models.py
@@ -1,3 +1,8 @@
+try:
+    from decimal import Decimal
+except:
+    from django.utils._decimal import Decimal
+
 from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext, ugettext_lazy as _
@@ -7,9 +12,21 @@ from satchmo.shop.models import Order
 class PurchaseOrder(models.Model):
     po_number = models.CharField(_('Customer PO Number'), max_length=20)
     order = models.ForeignKey(Order)
+    balance = models.DecimalField(_("Outstanding Balance"),
+        max_digits=18, decimal_places=10, blank=True, null=True)
+    paydate = models.DateField(_('Paid on'), blank=True, null=True)
+    notes = models.TextField(_('Notes'), blank=True, null=True)
     
     def __unicode__(self):
         return "PO: #%s" % self.po_number
+
+    def balance_due(self):
+        if self.balance:
+            b = self.balance
+        else:
+            b = Decimal('0.00')
+            
+        return moneyfmt(b)
 
     def order_link(self):
         return mark_safe(u'<a href="/admin/shop/order/%i/">%s #%i (%s)</a>' % (
@@ -17,8 +34,14 @@ class PurchaseOrder(models.Model):
             ugettext('Order'), 
             self.order.id, 
             moneyfmt(self.order.total)))
-             
+            
     order_link.allow_tags = True
+    
+    def save(self):
+        if self.balance is None:
+            self.balance = self.order.balance
+        super(PurchaseOrder, self).save()
+    
     
             
 
