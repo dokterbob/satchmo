8a12024b81e7593047c54fc0822b1c9cb6ebf19a
Bruce Kroeze <bkroeze@gmail.com>
2008-10-27 05:07:04 +0000

    fixing error with new tag
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401669

--- satchmo/shop/templatetags/satchmo_price.py
+++ satchmo/shop/templatetags/satchmo_price.py
@@ -28,6 +28,10 @@ def option_total_price(product, option_item):
     or (-$1.00) depending on the sign of the price change
     The currency symbol is set in the settings.py file
     """
-    return moneyfmt(product.unit_price + option_item.price_change)
+    if option_item.price_change:
+        val = product.unit_price + option_item.price_change
+    else:
+        val = product.unit_price
+    return moneyfmt(val)
 
 register.simple_tag(option_total_price)
