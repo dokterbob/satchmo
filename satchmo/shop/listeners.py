7dffc771ea73d0574e0db886d053184499db7710
Bruce Kroeze <bkroeze@gmail.com>
2008-11-21 21:24:20 +0000

    added new listener
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401715

--- satchmo/shop/listeners.py
+++ satchmo/shop/listeners.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from satchmo.shop.exceptions import OutOfStockError
+import logging
+
+log = logging.getLogger('shop.listeners')
 
 def veto_out_of_stock(sender, cartitem=None, added_quantity=0, **kwargs):
     """Listener which vetoes adding products to the cart which are out of stock."""
@@ -12,3 +15,10 @@ def veto_out_of_stock(sender, cartitem=None, added_quantity=0, **kwargs):
         need_qty = cartitem.quantity + added_quantity
         if product.items_in_stock < need_qty:
             raise OutOfStockError(product, product.items_in_stock, need_qty)
+
+
+def only_one_item_in_cart(sender, cart=None, cartitem=None, **kwargs):
+    for item in cart.cartitem_set.all():
+        if not item == cartitem:
+            log.debug('only one item in cart active: removing %s', item)
+            item.delete()
