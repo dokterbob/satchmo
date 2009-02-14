9cc9b6a496335028e8121a6f7656ad8ecb65a4ba
Chris Moffitt <chris@moffitts.net>
2009-02-14 14:49:39 +0000

    Changing the setlocale function to use utf-8. This should make this portion of the code more robust but we'll keep an eye on it. Closes #731
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%401955

--- satchmo/apps/l10n/utils.py
+++ satchmo/apps/l10n/utils.py
@@ -19,7 +19,7 @@ def get_locale_conv(loc=None):
 
     try:
         #log.debug('setting locale: %s', loc.encode('utf-8'))
-        locale.setlocale(locale.LC_ALL, locale.normalize(loc))
+        locale.setlocale(locale.LC_ALL, (loc,'utf-8'))
         return locale.localeconv()
     except (locale.Error, ValueError):
         # darn, try a different path
