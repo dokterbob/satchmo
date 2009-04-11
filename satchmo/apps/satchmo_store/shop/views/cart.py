259fdaf73dc5c4a49159033e67300676fad87fa5
Chris Moffitt <chris@moffitts.net>
2009-04-11 14:42:58 +0000

    Fix ajax cart code to support decimal quantities. Closes #779
    --HG--
    extra : convert_revision : svn%3Aa38d40e9-c014-0410-b785-c606c0c8e7de/satchmo/trunk%402020

--- satchmo/apps/satchmo_store/shop/views/cart.py
+++ satchmo/apps/satchmo_store/shop/views/cart.py
@@ -239,7 +239,7 @@ def add_ajax(request, id=0, template="shop/json.html"):
     else:
         data['results'] = _('Error')
 
-    data['cart_count'] = tempCart.numItems
+    data['cart_count'] = str(round_decimal(tempCart.numItems, 2))
 
     encoded = JSONEncoder().encode(data)
     encoded = mark_safe(encoded)
@@ -302,7 +302,7 @@ def remove_ajax(request, template="shop/json.html"):
         # note we have to convert Decimals to strings, since simplejson doesn't know about Decimals
         if cart and cartitem:
             data['cart_total'] = str(cart.total)
-            data['cart_count'] = cart.numItems
+            data['cart_count'] = str(round_decimal(tempCart.numItems, 2)) 
             data['item_id'] = cartitem.id
 
         return render_to_response(template, {'json': JSONEncoder().encode(data)})
