6e79990f66d03136ef7b621289f93cfa4b8ac8c3
Chris Moffitt <chris@moffitts.net>
2009-07-02 19:54:52 -0500

    Updating version info to use mercurial. Closes #833

--- satchmo/apps/satchmo_store/__init__.py
+++ satchmo/apps/satchmo_store/__init__.py
@@ -1,5 +1,5 @@
 VERSION = (0, 9, 'pre')
-
+            
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
     str_version = "%d.%d_%s" % VERSION[:3]
@@ -10,9 +10,18 @@ __version__ = str_version
 
 def get_version():
     "Returns the version as a human-format string."
+    hg_rev='hg-unknown'
     v = '.'.join([str(i) for i in VERSION[:-1]])
     if VERSION[-1]:
-        import satchmo_store
-        from django.utils.version import get_svn_revision
-        v = '%s-%s-%s' % (v, VERSION[-1], get_svn_revision(path=satchmo_store.__path__[0]))
+        try:
+            import os
+            from mercurial import ui, hg
+            dir = os.path.dirname(__file__)
+            hg_dir = os.path.normpath(os.path.join(dir,"../../../"))
+            repo = hg.repository(ui.ui(), hg_dir)
+            c = repo['tip']
+            hg_rev = "hg-%s:%s" % (c.rev(), c.hex()[0:12])
+        except:
+            pass
+        v = '%s-%s %s' % (v, VERSION[-1], hg_rev)
     return v
