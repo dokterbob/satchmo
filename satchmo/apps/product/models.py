ebf1be1fab9f95bd5c1568cd0f7ee2e75bfb2e52
Chris Moffitt <chris@moffitts.net>
2009-07-03 17:19:04 -0500

    Fix ordering of prices. Closes #835

--- satchmo/apps/product/models.py
+++ satchmo/apps/product/models.py
@@ -2216,7 +2216,7 @@ def get_product_quantity_adjustments(product, qty=1, parent=None):
 
     if qty_discounts.count() > 0:
         # Get the price with the quantity closest to the one specified without going over
-        adjustments = qty_discounts.order_by('-quantity')[0].adjustments()
+        adjustments = qty_discounts.order_by('-quantity', 'expires')[0].adjustments()
 
     elif parent:
         adjustments = get_product_quantity_adjustments(parent, qty=qty)
