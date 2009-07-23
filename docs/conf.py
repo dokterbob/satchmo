eb2a6a3491910d7623acaf26e6b10bbca0d451f4
Chris Moffitt <chris@moffitts.net>
2009-07-22 20:10:54 -0500

    Changing the sphinx docs so svn isn't mentioned.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ copyright = '2009, Chris Moffitt'
 # The short X.Y version.
 version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.9(svn)'
+release = '0.9(hg)'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
