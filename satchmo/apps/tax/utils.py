32629dae57dc99bcb75a1ca116f5371d22276a11
Bruce Kroeze <bkroeze@gmail.com>
2009-08-11 16:32:47 -0700

    removing unused import, accepting patch from #865

--- satchmo/apps/tax/utils.py
+++ satchmo/apps/tax/utils.py
@@ -1,16 +1,13 @@
 from livesettings import config_value
-from satchmo_utils import load_module, load_once
-try:
-    import decimal
-except ImportError:
-    from django.utils import _decimal as decimal    # for Python 2.3
+from satchmo_utils import load_module
+import decimal
 
 TWOPLACES = decimal.Decimal('0.01')
 
 def get_tax_processor(order=None, user=None):
-    modulename = config_value('TAX','MODULE')
-    mod = load_module(modulename + u'.processor')
-    return mod.Processor(order=order,user=user)
-    
+    modulename = config_value('TAX', 'MODULE')
+    mod = load_module(u'%s.processor' % modulename)
+    return mod.Processor(order=order, user=user)
+
 def round_cents(x):
     return x.quantize(TWOPLACES, decimal.ROUND_FLOOR)
