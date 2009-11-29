88425cc2f6401ea324d6be7cf97f063ca40c31c7
Chris Moffitt <chris@moffitts.net>
2009-11-29 16:07:29 -0600

    Adding an admin action for activating and deactivating countries in the admin.

--- satchmo/apps/l10n/admin.py
+++ satchmo/apps/l10n/admin.py
@@ -8,9 +8,29 @@ class AdminArea_Inline(admin.TabularInline):
     extra = 1
 
 class CountryOptions(admin.ModelAdmin):
-    list_display = ('printable_name', 'iso2_code',)
+    
+    def make_active(self, request, queryset):
+        rows_updated = queryset.update(active=True)
+        if rows_updated == 1:
+            message_bit = _("1 country was")
+        else:
+            message_bit = _("%s countries were" % rows_updated)
+        self.message_user(request, _("%s successfully marked as active") % message_bit)
+    make_active.short_description = _("Mark selected countries as active")
+    
+    def make_inactive(self, request, queryset):
+        rows_updated = queryset.update(active=False)
+        if rows_updated == 1:
+            message_bit = _("1 country was")
+        else:
+            message_bit = _("%s countries were" % rows_updated)
+        self.message_user(request, _("%s successfully marked as inactive") % message_bit)
+    make_inactive.short_description = _("Mark selected countries as inactive")
+    
+    list_display = ('printable_name', 'iso2_code','active')
     list_filter = ('continent', 'active')
     search_fields = ('name', 'iso2_code', 'iso3_code')
+    actions = ('make_active', 'make_inactive')
     inlines = [AdminArea_Inline]
 
 admin.site.register(Country, CountryOptions)
