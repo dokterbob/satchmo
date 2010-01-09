0c2cbf798c3209d5259ed37ccc5984f63a02dec7
Chris Moffitt <chris@moffitts.net>
2010-01-09 14:12:53 -0600

    Removing slug configuration from livesettings. Updating the docs on some other settings.

--- satchmo/apps/product/config.py
+++ satchmo/apps/product/config.py
@@ -40,20 +40,6 @@ We can handle it any which way."""),
         requiresvalue='product::DownloadableProduct'
     ),
 
-    StringValue(PRODUCT_GROUP,
-        'CATEGORY_SLUG',
-        description=_("Category Slug"),
-        help_text=_("The url slug for categories.  Requires server restart if changed."),
-        default="category"
-    ),
-    
-    StringValue(PRODUCT_GROUP,
-        'PRODUCT_SLUG',
-        description=_("Product Slug"),
-        help_text=_("The url slug for products.  Requires server restart if changed."),
-        default="product"
-    ),
-    
     PositiveIntegerValue(PRODUCT_GROUP,
         'NUM_DISPLAY',
         description=_("Total featured"),
--- satchmo/apps/satchmo_store/shop/satchmo_settings.py
+++ satchmo/apps/satchmo_store/shop/satchmo_settings.py
@@ -20,8 +20,9 @@ satchmo_settings_defaults = {
     'CUSTOM_TAX_MODULES' : [],
     'ALLOW_PRODUCT_TRANSLATIONS' : True,
     'COOKIE_MAX_SECONDS' : 60*60*24*30, #one month
-    'SHOW_TRANSLATIONS' : True, # show transaltions in admin
-    'ALLOW_TRANSLATIONS' : True # show available languages to users
+    'ALLOW_TRANSLATIONS' : True, # show available languages to users
+    'CATEGORY_SLUG': 'category', # Used for the category url
+    'PRODUCT_SLUG' : 'product', # Used for the product url
     }
 
 
