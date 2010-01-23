ef9420a75b4d23ea2c9acaff0da977b2d96d5c8a
Chris Moffitt <chris@moffitts.net>
2010-01-23 14:17:31 -0600

    Ensure that SettingNotSet exception propagates the error message correctly.

--- satchmo/apps/livesettings/models.py
+++ satchmo/apps/livesettings/models.py
@@ -74,6 +74,7 @@ class SettingNotSet(Exception):
     def __init__(self, k, cachekey=None):
         self.key = k
         self.cachekey = cachekey
+        Exception.__init__(self, k, cachekey)
 
 class SettingManager(models.Manager):
     def get_query_set(self):
