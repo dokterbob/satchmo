881ea7824e4bd889bf1c6f3b57003b4ad3080817
Tay Ray Chuan <rctay89@gmail.com>
2010-03-02 13:34:35 +0800

    satchmo_category: strip trailing whitespace

--- satchmo/apps/satchmo_store/shop/templatetags/satchmo_category.py
+++ satchmo/apps/satchmo_store/shop/templatetags/satchmo_category.py
@@ -1,4 +1,4 @@
-from django import template 
+from django import template
 from django.template import Library, Node, TemplateSyntaxError
 from product.models import Category
 from satchmo_utils.templatetags import get_filter_args
@@ -35,10 +35,10 @@ def recurse_for_children(current_node, parent_node, active_cat, show_empty=True)
 
 def category_tree(id=None):
     """
-    Creates an unnumbered list of the categories.  
-    
+    Creates an unnumbered list of the categories.
+
     Example::
-    
+
         <ul>
             <li>Books
                 <ul>
@@ -90,7 +90,7 @@ class CategoryListNode(Node):
         self.nodelist = nodelist
 
     def render(self, context):
-                   
+
         if self.slug:
             try:
                 cat = Category.objects.active().get(slug__iexact=self.slug.resolve(context))
@@ -98,18 +98,18 @@ class CategoryListNode(Node):
             except (Category.DoesNotExist, template.VariableDoesNotExist):
                 log.warn("No category found for slug: %s", self.slug)
                 cats = []
-        
+
         else:
             cats = Category.objects.root_categories()
-                    
+
         context[self.var] = cats
 
         context.push()
         context[self.var] = cats
         output = self.nodelist.render(context)
         context.pop()
-        return output            
-            
+        return output
+
 def do_categorylistnode(parser, token):
     """Push the category list into the context using the given variable name.
 
@@ -118,42 +118,42 @@ def do_categorylistnode(parser, token):
         {% category_list slug as var %}
         or
         {% category_list as var %}
-        
+
 
     """
     args = token.split_contents()
     ct = len(args)
     if not ct in (3,4):
         raise TemplateSyntaxError("%r tag expecting '[slug] as varname', got: %s" % (args[0], args))
-    
+
     if ct == 3:
         slug = None
         var = args[2]
     else:
         slug = template.Variable(args[1])
         var = args[3]
-        
+
     nodelist = parser.parse(('endcategory_list',))
     parser.delete_first_token()
 
     return CategoryListNode(slug, var, nodelist)
-    
+
 
 register.tag('category_list', do_categorylistnode)
 
 def product_category_siblings(product, args=""):
-    args, kwargs = get_filter_args(args, 
-        keywords=('variations', 'include_self'), 
-        boolargs=('variations', 'include_self'), 
+    args, kwargs = get_filter_args(args,
+        keywords=('variations', 'include_self'),
+        boolargs=('variations', 'include_self'),
         stripquotes=True)
-        
+
     sibs = product.get_category.product_set.all().order_by('ordering', 'name')
     if not kwargs.get('variations', True):
         sibs = [sib for sib in sibs if not sib.has_variants]
-    
+
     if not kwargs.get('include_self', True):
         sibs = [sib for sib in sibs if not sib == product]
-    
+
     return sibs
 
 register.filter('product_category_siblings', product_category_siblings)
