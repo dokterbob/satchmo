e32c7eaa560ae644a6285ad1474ea22ab4dc8c05
Tay Ray Chuan <rctay89@gmail.com>
2010-03-14 19:12:59 +0800

    strip trailing whitespace in some files

--- satchmo/apps/product/admin.py
+++ satchmo/apps/product/admin.py
@@ -21,7 +21,7 @@ class CategoryTranslation_Inline(admin.StackedInline):
 class CategoryImage_Inline(admin.TabularInline):
     model = CategoryImage
     extra = 3
-    
+
     formfield_overrides = {
         ImageWithThumbnailField : {'widget' : AdminImageWithThumbnailWidget},
     }
@@ -60,7 +60,7 @@ class Trial_Inline(admin.StackedInline):
     extra = 2
 
 class ProductAttributeForm(models.ModelForm):
-    
+
     def clean_validation(self):
         validation = self.cleaned_data['validation']
         try:
@@ -68,15 +68,15 @@ class ProductAttributeForm(models.ModelForm):
         except:
             raise ValidationError(_("Invalid regular expression"))
         return validation
-        
+
 
 class ProductAttributeAdmin(admin.ModelAdmin):
     form = ProductAttributeForm
     prepopulated_fields = {"name": ("description",)}
-    
+
 
 class ProductAttributeInlineForm(models.ModelForm):
-    
+
     def clean_value(self):
         value = self.cleaned_data['value']
         attribute = self.cleaned_data['option']
@@ -102,21 +102,21 @@ class Price_Inline(admin.TabularInline):
 class ProductImage_Inline(admin.StackedInline):
     model = ProductImage
     extra = 3
-    
+
     formfield_overrides = {
         ImageWithThumbnailField : {'widget' : AdminImageWithThumbnailWidget},
     }
- 
-class ProductTranslation_Inline(admin.TabularInline): 
-    model = ProductTranslation 
-    extra = 1    
+
+class ProductTranslation_Inline(admin.TabularInline):
+    model = ProductTranslation
+    extra = 1
 
 class ProductImageTranslation_Inline(admin.StackedInline):
     model = ProductImageTranslation
     extra = 1
 
 class CategoryAdminForm(models.ModelForm):
-    
+
     def clean_parent(self):
         parent = self.cleaned_data['parent']
         slug = self.cleaned_data['slug']
@@ -127,16 +127,16 @@ class CategoryAdminForm(models.ModelForm):
             for p in parent._recurse_for_parents(parent):
                 if slug == p.slug:
                     raise ValidationError(_("You must not save a category in itself!"))
-                    
+
         return parent
 
 class CategoryOptions(admin.ModelAdmin):
-    
+
     if config_value('SHOP','SHOW_SITE'):
         list_display = ('site',)
     else:
         list_display = ()
-    
+
     list_display += ('name', '_parents_repr', 'is_active')
     list_display_links = ('name',)
     ordering = ['site', 'parent__id', 'ordering', 'name']
@@ -175,7 +175,7 @@ class OptionOptions(admin.ModelAdmin):
         inlines.append(OptionTranslation_Inline)
 
 class ProductOptions(admin.ModelAdmin):
-    
+
     def make_active(self, request, queryset):
         rows_updated = queryset.update(active=True)
         if rows_updated == 1:
@@ -185,7 +185,7 @@ class ProductOptions(admin.ModelAdmin):
         self.message_user(request, _("%s successfully marked as active") % message_bit)
         return HttpResponseRedirect('')
     make_active.short_description = _("Mark selected products as active")
-    
+
     def make_inactive(self, request, queryset):
         rows_updated = queryset.update(active=False)
         if rows_updated == 1:
@@ -195,7 +195,7 @@ class ProductOptions(admin.ModelAdmin):
         self.message_user(request, _("%s successfully marked as inactive") % message_bit)
         return HttpResponseRedirect('')
     make_inactive.short_description = _("Mark selected products as inactive")
-    
+
     def make_featured(self, request, queryset):
         rows_updated = queryset.update(featured=True)
         if rows_updated == 1:
@@ -205,7 +205,7 @@ class ProductOptions(admin.ModelAdmin):
         self.message_user(request, _("%s successfully marked as featured") % message_bit)
         return HttpResponseRedirect('')
     make_featured.short_description = _("Mark selected products as featured")
-    
+
     def make_unfeatured(self, request, queryset):
         rows_updated = queryset.update(featured=False)
         if rows_updated == 1:
@@ -226,21 +226,21 @@ class ProductOptions(admin.ModelAdmin):
     list_filter = ('category', 'date_added','active','featured')
     actions = ('make_active', 'make_inactive', 'make_featured', 'make_unfeatured')
     fieldsets = (
-    (None, {'fields': ('site', 'category', 'name', 'slug', 'sku', 'description', 'short_description', 'date_added', 
-            'active', 'featured', 'items_in_stock','total_sold','ordering', 'shipclass')}), (_('Meta Data'), {'fields': ('meta',), 'classes': ('collapse',)}), 
-            (_('Item Dimensions'), {'fields': (('length', 'length_units','width','width_units','height','height_units'),('weight','weight_units')), 'classes': ('collapse',)}), 
-            (_('Tax'), {'fields':('taxable', 'taxClass'), 'classes': ('collapse',)}), 
+    (None, {'fields': ('site', 'category', 'name', 'slug', 'sku', 'description', 'short_description', 'date_added',
+            'active', 'featured', 'items_in_stock','total_sold','ordering', 'shipclass')}), (_('Meta Data'), {'fields': ('meta',), 'classes': ('collapse',)}),
+            (_('Item Dimensions'), {'fields': (('length', 'length_units','width','width_units','height','height_units'),('weight','weight_units')), 'classes': ('collapse',)}),
+            (_('Tax'), {'fields':('taxable', 'taxClass'), 'classes': ('collapse',)}),
             (_('Related Products'), {'fields':('related_items','also_purchased'),'classes':('collapse',)}), )
     search_fields = ['slug', 'sku', 'name']
     inlines = [ProductAttribute_Inline, Price_Inline, ProductImage_Inline]
     if get_l10n_setting('show_translations'):
         inlines.append(ProductTranslation_Inline)
     filter_horizontal = ('category',)
-    
+
     def formfield_for_dbfield(self, db_field, **kwargs):
         field = super(ProductOptions, self).formfield_for_dbfield(db_field, **kwargs)
         fieldname = db_field.name
-        if fieldname in ("length_units", "width_units", "height_units"): 
+        if fieldname in ("length_units", "width_units", "height_units"):
             field.initial = default_dimension_unit()
         elif fieldname == "weight_units":
             field.initial = default_weight_unit()
