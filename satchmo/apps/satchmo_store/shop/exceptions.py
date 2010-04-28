b11980bc747b87f3b34afaa221eb57b153bef844
Johannes Bornhold <joh@bo-tech.de>
2010-04-28 19:02:06 +0200

    cleanup of unused import

--- satchmo/apps/satchmo_store/shop/exceptions.py
+++ satchmo/apps/satchmo_store/shop/exceptions.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _, ugettext
+from django.utils.translation import ugettext_lazy as _
 
 class CartAddProhibited(Exception):
     """Raised when a `signals.satchmo_cart_add_verify` listener vetoes adding an item to the cart.
