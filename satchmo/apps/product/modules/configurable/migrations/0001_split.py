cc678ed894e0645f73f0963c3ce81b4495edeaa4
Tay Ray Chuan <rctay89@gmail.com>
2010-05-15 22:01:05 +0800

    product/migrations: sort renames alphabetically

--- satchmo/apps/product/modules/configurable/migrations/0001_split.py
+++ satchmo/apps/product/modules/configurable/migrations/0001_split.py
@@ -6,14 +6,14 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         db.rename_table('product_configurableproduct', 'configurable_configurableproduct')
+        db.rename_table('product_configurableproduct_option_group', 'configurable_configurableproduct_option_group')
         db.rename_table('product_productvariation', 'configurable_productvariation')
         db.rename_table('product_productvariation_options', 'configurable_productvariation_options')
-        db.rename_table('product_configurableproduct_option_group', 'configurable_configurableproduct_option_group')
 
     def backwards(self, orm):
         db.rename_table('configurable_configurableproduct', 'product_configurableproduct')
+        db.rename_table('configurable_configurableproduct_option_group', 'product_configurableproduct_option_group')
         db.rename_table('configurable_productvariation', 'product_productvariation')
         db.rename_table('configurable_productvariation_options', 'product_productvariation_options')
-        db.rename_table('configurable_configurableproduct_option_group', 'product_configurableproduct_option_group')
 
     complete_apps = ['configurable']
--- satchmo/apps/product/modules/custom/migrations/0001_split.py
+++ satchmo/apps/product/modules/custom/migrations/0001_split.py
@@ -6,14 +6,14 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         db.rename_table('product_customproduct', 'custom_customproduct')
+        db.rename_table('product_customproduct_option_group', 'custom_customproduct_option_group')
         db.rename_table('product_customtextfield', 'custom_customtextfield')
         db.rename_table('product_customtextfieldtranslation', 'custom_customtextfieldtranslation')
-        db.rename_table('product_customproduct_option_group', 'custom_customproduct_option_group')
 
     def backwards(self, orm):
         db.rename_table('custom_customproduct', 'product_customproduct')
+        db.rename_table('custom_customproduct_option_group', 'product_customproduct_option_group')
         db.rename_table('custom_customtextfield', 'product_customtextfield')
         db.rename_table('custom_customtextfieldtranslation', 'product_customtextfieldtranslation')
-        db.rename_table('custom_customproduct_option_group', 'product_customproduct_option_group')
 
     complete_apps = ['custom']
