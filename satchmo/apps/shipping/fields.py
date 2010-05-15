621edbd163e39b1b9b50907d5836eb4699498869
Tay Ray Chuan <rctay89@gmail.com>
2010-05-15 20:57:48 +0800

    shipping.fields: strip trailing whitespace

--- satchmo/apps/shipping/fields.py
+++ satchmo/apps/shipping/fields.py
@@ -5,13 +5,13 @@ def shipping_choices():
     try:
         return config_choice_values('SHIPPING','MODULES')
     except SettingNotSet:
-        return ()    
+        return ()
 
 
 class ShippingChoiceCharField(models.CharField):
-    
+
     def __init__(self, choices="__DYNAMIC__", *args, **kwargs):
         if choices == "__DYNAMIC__":
             kwargs['choices'] = shipping_choices()
-                    
+
         super(ShippingChoiceCharField, self).__init__(*args, **kwargs)
