a978edca6ff3316542c94860e516cdd0f70f681e
Dinh Bowman <dinh.bowman@gmail.com>
2010-08-25 17:15:29 -0700

    Fixed persistent cart bug which causes the user's current (non-empty) cart to be ignored and switched for an empty cart.

--- satchmo/apps/satchmo_store/shop/config.py
+++ satchmo/apps/satchmo_store/shop/config.py
@@ -63,8 +63,16 @@ config_register(PositiveIntegerValue(
 PERSISTENT_CART = config_register(
     BooleanValue(SHOP_GROUP,
     'PERSISTENT_CART',
-    description = _('Persistent Cart?'),
-    help_text = _("When a user logs in, attempt to retrieve previous carts and merge with existing?"),
+    description = _('Persistent Cart: Check for old cart?'),
+    help_text = _("When a user logs in, attempt to retrieve previous cart?"),
+    default = False
+    ))
+
+PERSISTENT_CART_MERGE = config_register(
+    BooleanValue(SHOP_GROUP,
+    'PERSISTENT_CART_MERGE',
+    description = _('Persistent Cart: Overrides Current Cart?'),
+    help_text = _("If you have a persistent cart: Should you merge the carts even if the user has created a new one?"),
     default = False
     ))
 
